b8a75dde98992e21978a56328d2cab2a931c572e
SCNomeD <vladiiimir1991@gmail.com>
2022-02-20 16:24:35 +0300

    Unit 5.2.9/Проблема!

--- Unit_1_2(2)/Program.cs
+++ Unit_1_2(2)/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Unit_1_2
+namespace Unit_1_2_2_
 {
     class Program
     {
@@ -75,27 +75,25 @@ namespace Unit_1_2
             #region
             //GetArrayFromConsole();
             //Console.WriteLine();
-
-            //var (name, age) = ("Евгения", 27);
-
-            //Console.WriteLine("Мое имя: {0}", name);
-            //Console.WriteLine("Мой возраст: {0}", age);
             #endregion
 
-            (string name, int age) anketa;
+            var (name, age) = ("Евгения", 27);
+
+            Console.WriteLine("Мое имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
 
             Console.Write("Введите имя: ");
-            anketa.name = Console.ReadLine();
+            name = Console.ReadLine();
             Console.Write("Введите возрас с цифрами:");
-            anketa.age = Convert.ToInt32(Console.ReadLine());
+            age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ваше имя: {0}", anketa.name);
-            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
 
             var favcolors = new string[3];
             for (int i = 0; i < favcolors.Length; i++)
             {
-                favcolors[i] = ShowColor(anketa.name, anketa.age);
+                favcolors[i] = ShowColor(name, age);
             }
 
             Console.WriteLine("Ваши любимые цвета:");
