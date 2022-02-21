b434ebfad66b3de622144ad33e204133f518de4c
SCNomeD <vladiiimir1991@gmail.com>
2022-02-21 22:49:28 +0300

    Корректировка 1

--- Unit_TEST/Program.cs
+++ Unit_TEST/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Unit_1_2
+namespace Unit_TEST
 {
     class Program
     {
@@ -38,45 +38,10 @@ namespace Unit_1_2
         static string GetDataFromConsole() => Console.ReadLine();
         static void ShowColor(params string[] favcolors)
         {
-            #region
-            //Console.WriteLine("{0} {1} лет\nНапишите свой любимый цвет на английском с маленькой буквы", username, userage);
-            //var color = Console.ReadLine();
-
-            //switch (color)
-            //{
-            //    case "red":
-            //        Console.BackgroundColor = ConsoleColor.Red;
-            //        Console.ForegroundColor = ConsoleColor.Black;
-
-            //        Console.WriteLine("Your color is red!");
-            //        break;
-
-            //    case "green":
-            //        Console.BackgroundColor = ConsoleColor.Green;
-            //        Console.ForegroundColor = ConsoleColor.Black;
-
-            //        Console.WriteLine("Your color is green!");
-            //        break;
-            //    case "cyan":
-            //        Console.BackgroundColor = ConsoleColor.Cyan;
-            //        Console.ForegroundColor = ConsoleColor.Black;
-
-            //        Console.WriteLine("Your color is cyan!");
-            //        break;
-            //    default:
-            //        Console.BackgroundColor = ConsoleColor.Yellow;
-            //        Console.ForegroundColor = ConsoleColor.Red;
-
-            //        Console.WriteLine("Your color is yellow!");
-            //        break;
-            //}
-            #endregion
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favcolors)
             {
-                Console.WriteLine("Ваши любимые цвета:");
-                foreach (var color in favcolors)
-                {
-                    Console.WriteLine(color);
-                }
+                Console.WriteLine(color);
             }
         }
         static void Main(string[] args)
@@ -90,6 +55,7 @@ namespace Unit_1_2
             //Console.WriteLine("Мое имя: {0}", name);
             //Console.WriteLine("Мой возраст: {0}", age);
             #endregion
+
             (string name, int age) anketa;
 
             Console.Write("Введите имя: ");
@@ -100,13 +66,15 @@ namespace Unit_1_2
             Console.WriteLine("Ваше имя: {0}", anketa.name);
             Console.WriteLine("Ваш возраст: {0}", anketa.age);
 
-            var favcolor = new string[3];
-            for (int i = 0; i < favcolor.Length; i++)
+            ShowColor();
+
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favcolors)
             {
-                ShowColor();
+                Console.WriteLine(color);
             }
 
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
     }
 }
