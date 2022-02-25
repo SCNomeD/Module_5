1d876cc540ede6f4367e12595b1b782fb48d0557
SCNomeD <vladiiimir1991@gmail.com>
2022-02-25 19:46:22 +0300

    Начал Unit_5

---
+++ Unit_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Unit_5
+{
+    class Program
+    {
+		static void Echo(string saidworld, int deep)
+		{
+			var modif = saidworld;
+
+			if (modif.Length > 2)
+			{
+				modif = modif.Remove(0, 2);
+			}
+
+			Console.WriteLine("..." + modif);
+
+			if (deep > 1)
+			{
+				Echo(modif, deep - 1);
+			}
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Напишите что-то");
+			var str = Console.ReadLine();
+
+			Console.WriteLine("Укажите глубину эха");
+			var deep = int.Parse(Console.ReadLine());
+
+			Echo(str, deep);
+
+			Console.ReadKey();
+		}
+	}
+}
